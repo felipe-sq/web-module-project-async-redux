450f0d004c36bb8a53e8778629396fa9da201b94
Felipe Quintero <ffquint@gmail.com>
2021-06-22 22:12:13 -0400

    worked on more app styling using both react-bootstrap and styled components.

--- app/src/components/Joke.js
+++ app/src/components/Joke.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { fetchJoke } from "../actions/jokeActions";
 import { Container, Card, Button, Badge } from 'react-bootstrap';
+import styled from 'styled-components';
 
 const Joke = (props) => {
   useEffect(() => { props.fetchJoke(); }, []);
@@ -9,12 +10,14 @@ const Joke = (props) => {
     return <><h2>Loading joke... </h2></>
   }
   return (
-    <Container>
-      <Card>
-        <Card.Title> {props.joke} </Card.Title><br/>
-        <button className="joke-btn" onClick={props.fetchJoke}>Get new Joke!</button>
-      </Card>
-    </Container>
+    <MainDiv>
+      <Container>
+        <Card>
+          <h2>{props.joke}</h2><br/>
+          <button className="joke-btn" onClick={props.fetchJoke}>Get new Joke!</button>
+        </Card>
+      </Container>
+    </MainDiv>
   )
 }
 
@@ -27,3 +30,16 @@ const mapStateToProps = (state) => {
 }
 
 export default connect(mapStateToProps, {fetchJoke})(Joke);
+
+const MainDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  border-radius: 6px;
+  margin: 36px 80px;
+  padding: 16px;
+  background-color: white;
+  width: auto;
+`;
--- app/src/index.js
+++ app/src/index.js
@@ -7,27 +7,46 @@ import logger from "redux-logger";
 import Joke from './components/Joke';
 import { jokeReducer } from './reducers/jokeReducer';
 import './index.css';
+import styled from 'styled-components';
+
 import thunk from 'redux-thunk';
-import { Container, Card, Col, Image } from 'react-bootstrap';
+import { Container, Card, Col, Image, Badge } from 'react-bootstrap';
 
 
 const store = createStore(jokeReducer, applyMiddleware(logger, thunk));
-
+const MainDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  border-radius: 6px;
+  margin: 36px 105px;
+  padding: 16px;
+  background-color: white;
+  width: auto;
+`;
 function App() {
   return (
     <div className="App">
-      <Container>
-        <Card>
-          <Card.Header as="h1">Random Chuck Norris Joke Generator</Card.Header>
-          <Card.Body>Enjoy a chuckle or two!</Card.Body><br/>
-          <Col xs={1} md={2} >
-            <Image className="d-block mx-auto img-fluid w-10" src="chuck-norris.png" rounded fluid/><br/>
-            </Col>
-        </Card>
-        <Card as="h5" className="text-center">
-          <Joke />
-        </Card>
-      </Container>
+      <MainDiv>
+        <Container>
+              <h1>Random Chuck Norris Joke Generator</h1>  
+          <Card>
+            <Card.Body>Enjoy a chuckle or two!</Card.Body><br/>
+            <Col xs={1} md={2} >
+              <Image className="d-block mx-auto img-fluid w-10" src="chuck-norris.png" rounded fluid/><br/>
+              </Col>
+          </Card>
+          </Container>
+        </MainDiv>
+        <MainDiv>
+          <Container>
+          <Card className="text-center">
+            <Joke />
+          </Card>
+        </Container>
+      </MainDiv>
     </div>
   );
 }
